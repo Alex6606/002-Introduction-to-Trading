0d70a3a78f5480d4637479244a814910ccc8edb0
Alex6606 <alex.saji06@gmail.com>
2025-09-26 15:00:58 -0600

    add basic stuff

---
+++ Main.py
@@ -0,0 +1,10 @@
+#Entrypoint
+from utils import sort_list
+
+
+def main():
+    xs=[1,5,3,2,4]
+    print(sort_list(xs))
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,2 @@
+def sort_list(xs: list[int]) -> list[int]:
+    return sorted(xs)
